b52891917d2e3699e671cd0e56e1447827b4d596
taianaazevedo <taianazev@gmail.com>
2022-10-31 16:45:15 -0300

    funções funcionando

--- script.js
+++ script.js
@@ -1,72 +1,45 @@
 let usuario;
-let mensagem = [];
 
+
+// função que pergunta o nome do usuário:
 function perguntaNome() {
-    let nome = prompt('Qual seu nome?');
-    usuario = { name: `${nome}` };
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuario);
+    usuario = prompt('Qual seu nome?');
+    const usuarioCadastrado = { name: usuario };
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuarioCadastrado);
     promise.then(processaNomeCerto);
     promise.catch(processaNomeErrado);
 }
-perguntaNome()
+perguntaNome();
 
-function processaNomeCerto(resposta) {
-       alert("Você está online! Bem-vindo(a) ao Bate-Papo UOL!");    
+function processaNomeCerto() {
+       alert("Você está online! Bem-vindo(a) ao Bate-Papo UOL!");   
+       continuaOnline();
 }
-function processaNomeErrado(resposta) { 
+
+function processaNomeErrado() { 
     alert("Já existe um usuário com esse nome. Insira um novo nome.");
     prompt("Qual seu nome?");
 }
 
+// função que verifica se o usuário ainda está online:
 function continuaOnline(){
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuario)
-    promise.then(aindaEstaOnline);
-    promise.catch(estaOffline);
+    const usuarioOnline = { name: usuario };
+    axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuarioOnline)
 }
-/*setInterval(continuaOnline, 10000);*/
+setInterval(continuaOnline, 5000); 
 
-function aindaEstaOnline(resposta){
-    console.log(resposta.data);
-}
-function estaOffline(resposta){
-    console.log(resposta.data);
-    alert('Você ficou muito tempo fora. Logue novamente.');
-    perguntaNome();
-}
 
+//função que busca as mensagens no servidor:
 function buscarMensagens() {
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promise.then(mensagensChat);
-    promise.catch(mensagensChatErro);
-    
+    promise.then(renderizarMensagens); 
 }
 buscarMensagens();
-/*setInterval(buscarMensagens, 3000);*/
+setInterval(buscarMensagens, 3000);
 
-function mensagensChat(resposta){
-    mensagem = resposta.data;
-
-    renderizarMensagens();
-}
-mensagensChat();
-
-function mensagensChatErro(resposta){
-    console.log(resposta)
-}
-/*function mensagensChat(resposta){
-    mensagem = [];
-    for(i = 0; i < resposta.data.length; i++){
-        const mensagensApi = resposta.data[i];
-        mensagem.push(mensagensApi);
-    }
-       console.log(resposta)
-    renderizarMensagens();
-}
-mensagensChat();*/
-
-
-
-function renderizarMensagens(){
+// função que renderiza as mensagens vindas do servidor no html:
+function renderizarMensagens(resposta){
+    const mensagem = resposta.data;
     const telaDoChat = document.querySelector('.mensagem-chat');
     telaDoChat.innerHTML = '';
     for(i = 0; i < mensagem.length; i++){
@@ -74,33 +47,59 @@ function renderizarMensagens(){
 
         if(mensagens.type === "status"){
             telaDoChat.innerHTML +=
-                `<div class="mensagem-chat status">
-                    <span class="hora">${mensagens.time}&nbsp;</span>                
-                    <span class="locutor"><b>${mensagens.from}</b></span>                                    
-                </div>`
+                `<div class="status">
+                    <span class="hora">(${mensagens.time})&nbsp;</span>                
+                    <span class="locutor"><b>${mensagens.from}</b></span>
+                    <span class="texto">${mensagens.text}</span>                                   
+                </div>`;
         } else if (mensagens.type === "private_message"){
+            if(mensagens.to === usuario || mensagens.to === "Todos" || mensagens.from === usuario){
             telaDoChat.innerHTML +=
-                `<div class="mensagem-chat reservada">
-                    <span class="hora">${mensagens.time}&nbsp;</span>                
+                `<div class="reservada">
+                    <span class="hora">(${mensagens.time})&nbsp;</span>                
                      <span class="locutor"><b>${mensagens.from}</b></span>
-                     <span class="destinatario"><b>para <b>${mensagens.to}</b>&nbsp;</b></span>                    
+                     <span class="destinatario">reservadamente para <b>${mensagens.to}</b>&nbsp;</b></span>                    
                      <span class="texto">${mensagens.text}</span>                                    
-                 </div>`
+                 </div>`;
+                }
         } else if (mensagens.type === "message") {
             telaDoChat.innerHTML +=
-                `<div class="mensagem-chat normal">
-                    <span class="hora">${mensagens.time} &nbsp;</span>                
+                `<div class="normal">
+                    <span class="hora">(${mensagens.time})&nbsp;</span>                
                      <span class="locutor"><b>${mensagens.from}</b></span>
-                     <span class="destinatario"><b>para <b>${mensagens.to}</b>&nbsp;</b></span>                    
+                     <span class="destinatario">para <b>${mensagens.to}</b>&nbsp;</b></span>                    
                      <span class="texto">${mensagens.text}</span>                                    
-                 </div>`
+                 </div>`;
 
         }
     }
-
+    const ultimaMsgChat = document.querySelector('.mensagem-chat div:last-child');
+    ultimaMsgChat.scrollIntoView();
+    
 }
 renderizarMensagens();
 
-/*function alerta(){
-    alert("oi");  // teste do OnClick de enviar mensagem
-}*/
+
+//função que envia as mensagens digitadas no input:
+function enviarMensagem(){    
+    const input = document.querySelector('input');
+    const textoMsg = input.value;
+    const mensagemEscrita = {
+	    from: usuario,
+	    to: "Todos",
+	    text: textoMsg,
+	    type: "message" // ou "private_message" para o bônus
+    }        
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagemEscrita);
+    promise.then(buscarMensagens);
+    promise.catch(recarregaChat);
+
+    input.value = "";
+
+}
+enviarMensagem();
+
+function recarregaChat(){
+    window.location.reload();
+}
+
